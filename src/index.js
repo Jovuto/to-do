4a234fe73f87d67c2af2d0ca968f2cb5a831bb08
Jovuto <153099429+Jovuto@users.noreply.github.com>
2025-02-06 12:07:09 +0000

    feat: add a complete constructor, array and a way to mark toDos as complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 let toDoObjectList = [];
+let completedToDos = [];
 
 class toDoManager {
 
     static createToDo(title) {
         toDoObjectList.push(new toDo(title));
+    }
+
+    static setComplete(number) {
+        toDoObjectList[number].complete = true;
+        completedToDos.push(toDoObjectList[number]);
+        toDoObjectList.splice(number, 1);
     }
     
 }
@@ -15,6 +22,7 @@ class toDo {
         this.dueDate = dueDate;
         this.priority = priority;
         this.checklist = checklist;
+        this.complete = false;
     }
 }
 
